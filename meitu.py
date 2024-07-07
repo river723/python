6de88d3c948a3afc446bcaec6680c5c9b75056c2
river723 <hewengong@live.cn>
2024-07-07 11:29:23 +0800

    得到青模网图片链接并下载为本地IMG文件，使用多线程

--- meitu.py
+++ meitu.py
@@ -8,17 +8,18 @@ from multiprocessing import Process,Queue
 # https://www.igdcc.com/shouji/fengjing/
 
 def getPicUrl(q):
-    url = 'https://www.igdcc.com/shouji/fengjing/'
+    url = 'https://3d.qingmo.com/'
     response = requests.get(url)
     response.encoding = 'utf-8'
     tree = etree.HTML(response.text)
-    picUrl_list = tree.xpath('//div[@class="mt15 clearfix pic-auto pic-list"]/a/@href')
+    picUrl_list = tree.xpath('//div[@class="list_img_item"]//a/@url')
     for i in picUrl_list:
         href=parse.urljoin(url,i)
+        print(href)
         child_response = requests.get(href)
         child_response.encoding = 'utf-8'
         child_tree = etree.HTML(child_response.text)
-        src=child_tree.xpath('//div[@class="preview-pic"]//a/img/@src')
+        src=child_tree.xpath('//a[@class="show_l_wrap__a fullsizable-a is-active"]/@href')[0]
         q.put(src)
     q.put("over")
 def download(url):
@@ -27,14 +28,16 @@ def download(url):
     name = url.split('/')[-1]
     with open('./img/'+name,mode='wb') as f:
         f.write(response.content)
-    print("下载完成")
+
+    print(name+"下载完成")
 def dowload_img(q):
-    with ThreadPoolExecutor(10) as executor:
+    with ThreadPoolExecutor(10) as t:
         while True:
             url = q.get()
+            print(url+"从队列取出\n")
             if url == "over":
                 break
-            executor.submit(download, url)
+            t.submit(download, url)
 
 
 if __name__ == '__main__':
@@ -44,3 +47,5 @@ if __name__ == '__main__':
     p1.start()
     p2.start()
 
+# https://img.qingmo.com/21080/2024/06/03/bccf02a221a811efb253005056a64936.jpg
+# https://img.qingmo.com/11080/2024/06/03/bccf02a221a811efb253005056a64936.jpg
