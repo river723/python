2dbf0e0dc59bd760f4729477ec46617f4c489f8a
river723 <hewengong@live.cn>
2024-07-12 17:57:23 +0800

    使用异步方法，得到小说的全部章节内容,完成

--- book.py
+++ book.py
@@ -1,44 +1,51 @@
+import aiofiles
 import requests
 import asyncio
+import aiofiles
 from lxml import etree
 from urllib import parse
 import aiohttp
 
-def parser_join(url,list):
+
+def parser_join(url, list):
     for i in list:
-        i=url+i
+        i = url + i
         yield i
+
+
 def get_everyone_url(url):
-    req=requests.get(url)
-    req.encoding='gbk'
-    html=req.text
-    tree=etree.HTML(html)
-    url_list=tree.xpath('//ul[@id="section-list"]/li/a/@href')
-    url_list_all=parser_join(url,url_list)
+    req = requests.get(url)
+    req.encoding = 'gbk'
+    html = req.text
+    tree = etree.HTML(html)
+    url_list = tree.xpath('//ul[@id="section-list"]/li/a/@href')
+    url_list_all = parser_join(url, url_list)
     return url_list_all
+
 
 async def down_book(url):
     async with aiohttp.ClientSession() as session:
-
         async with session.get(url) as resp:
-
-            html=await resp.text(encoding='gbk')
-            tree=etree.HTML(html)
-            book_name=tree.xpath('//div[@class="section-opt"]/h1/text()')[0]
-            content=tree.xpath('//div[@id="content"]/text()')[0]
-            async with open('./book/'+book_name+'.txt','w',encoding='utf-8') as f:
-                 await f.write(content)
+            html = await resp.text(encoding='gbk')
+            tree = etree.HTML(html)
+            book_name = " ".join(tree.xpath('//div[@class="reader-main"]/h1/text()'))
+            content = ' '.join(tree.xpath('//div[@id="content"]/text()'))
+            # print(book_name)
+            # print('--------------------')
+            # print(content)
+            async with aiofiles.open('./book/' + book_name + '.txt', 'w', encoding='utf-8') as f:
+                await f.write(content)
+    print(book_name, '下载完成')
 
 async def main():
-    url='https://www.biquge635.com/book/53041/'
-    url_list=get_everyone_url(url)
-    tasks=[]
+    url = 'https://www.biquge635.com/book/53041/'
+    url_list = get_everyone_url(url)
+    tasks = []
     for url in url_list:
-        t=asyncio.create_task(down_book(url))
+        t = asyncio.create_task(down_book(url))
         tasks.append(t)
-        break
+        # break
     await asyncio.wait(tasks)
-
 
 
 if __name__ == '__main__':
